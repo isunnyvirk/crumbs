eea28f9fd05e596fbd7f44e16f208d9491db0a88
isunnyvirk <isunnyvirk@gmail.com>
2016-07-28 22:11:02 -0700

    commit for deploy

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,8 @@ import App from './components/App';
 import io from 'socket.io-client';
 
 const mainSocket = io();
+// change for deploy
+var hello = 'hi';
 
 ReactDOM.render(
   <App mainSocket={mainSocket} />,
